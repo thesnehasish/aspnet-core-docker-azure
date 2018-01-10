4d1078c79b4373a08bf8830f2ae54a76f9f85cda
thesnehasish <thesnehasish@gmail.com>
2018-01-10 23:39:03 +0530

    Init version

---
+++ aspnet-core-docker-azure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnet_core_docker_azure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
